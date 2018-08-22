4125114305e26e05c401eada30551cdac462c7a4
libres <anto@libres.home>
2018-08-22 23:08:00 +0200

    New simple command

---
+++ commands/bonjour.js
@@ -0,0 +1,22 @@
+let config = require('../storage/config.json');
+let functions = require("../storage/functions.js");
+
+module.exports.run = async (bot, message, splitMessage) => {
+  message.delete();
+
+  if(!(message.channel.id === config.salonBotId)) {
+    return functions.sendError(message,
+      `${message.author} Merci d\'utiliser le salon \`${config.salonBot}\` pour les commande de Bot, *manche à couilles*`, true);
+  }
+
+  functions.sendEmbed(message, `Bonjour @everyone`, true);
+
+}
+
+module.exports.help = {
+  name: "",
+  category: "",
+  usage: `${config.prefix}`,
+  description: "",
+  aliases: ['']
+}
